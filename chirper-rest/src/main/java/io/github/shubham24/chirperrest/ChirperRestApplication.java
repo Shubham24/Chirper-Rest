57b486efd9452c912795355182b28ddf4436d4d5
Shubham Sinha <sinha.201@osu.edu>
2023-02-01 18:41:13 -0500

    initial commit

---
+++ chirper-rest/src/main/java/io/github/shubham24/chirperrest/ChirperRestApplication.java
@@ -0,0 +1,13 @@
+package io.github.shubham24.chirperrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChirperRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChirperRestApplication.class, args);
+	}
+
+}
---
+++ chirper-rest/src/test/java/io/github/shubham24/chirperrest/ChirperRestApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.shubham24.chirperrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChirperRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
